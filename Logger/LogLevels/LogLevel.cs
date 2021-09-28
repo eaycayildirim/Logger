67b8c2bf8e51456ee308596f15eec28fb6002a8b
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-09-28 11:12:29 +0300

    Refactoring

--- Logger/LogLevels/LogLevel.cs
+++ Logger/LogLevels/LogLevel.cs
@@ -9,8 +9,13 @@ namespace nsLogLevel
         public void Write(string message)
         {
             Log log = new Log();
-            var data = "[" + Name + " " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "]: " + message;
+            var data = GetLogData() + message;
             log.Write(data);
+        }
+
+        private string GetLogData()
+        {
+            return "[" + Name + " " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "]: ";
         }
 
         public string Name { get; set; }
